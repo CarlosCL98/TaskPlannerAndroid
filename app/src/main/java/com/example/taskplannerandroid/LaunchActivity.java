39ca015f706a1a107c0c8ab7741d4fec183dd648
unknown <2125262@labinfo.is.escuelaing.edu.co>
2019-11-13 17:25:39 -0500

    Adelanto el laboratorio de TaskPlanner en Android.

---
+++ app/src/main/java/com/example/taskplannerandroid/LaunchActivity.java
@@ -0,0 +1,32 @@
+package com.example.taskplannerandroid;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    public static final String TOKEN_KEY = "TOKEN_KEY";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SharedPreferences sharedPref =
+                getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        if (sharedPref.contains(TOKEN_KEY)) {
+            //TODO go to MainActivity
+            setContentView(R.layout.activity_main);
+        } else {
+            //TODO go to LoginActivity
+            setContentView(R.layout.activity_login);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/taskplannerandroid/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.taskplannerandroid;
+
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+}
